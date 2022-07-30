a1b083a6b4ee733dc48e246566818148d18fd14f
Chethumn-m <110308934+Chethumn-m@users.noreply.github.com>
2022-07-30 16:43:40 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Mana Tree",
+	id: "manatree",
+	author: "Chethumn-m",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
